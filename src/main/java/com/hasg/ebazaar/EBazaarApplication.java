b9d36d1fa5730deed5c7203fe1d63608f14e3a0c
HarnoorBajwa <hr569149@dal.ca>
2024-02-28 20:35:26 -0400

    deleted submodule and transferred files to the parent git repo

---
+++ src/main/java/com/hasg/ebazaar/EBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.hasg.ebazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBazaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBazaarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasg/ebazaar/EBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasg.ebazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
